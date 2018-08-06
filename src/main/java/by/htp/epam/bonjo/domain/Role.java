a241fbac236702496b69f7b06ced94fd7245375c
George <georgekrasutski@gmail.com>
2018-08-06 10:35:32 +0300

    role entity added

---
+++ src/main/java/by/htp/epam/bonjo/domain/Role.java
@@ -0,0 +1,70 @@
+package by.htp.epam.bonjo.domain;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = -608201873958322100L;
+
+	private int id;
+	private String role;
+
+	public Role(int id, String role) {
+		super();
+		this.id = id;
+		this.role = role;
+	}
+
+	public Role() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id != other.id)
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Role{" + "ID=" + id + ", Role='" + role + '\'' + '}';
+	}
+
+}

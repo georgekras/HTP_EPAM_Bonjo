2da429959286aebe4d10fadd00468996800f2494
George <georgekrasutski@gmail.com>
2018-08-20 13:39:16 +0300

    main command updated

--- src/main/java/by/htp/epam/bonjo/web/command/impl/MainPageCommand.java
+++ src/main/java/by/htp/epam/bonjo/web/command/impl/MainPageCommand.java
@@ -4,18 +4,24 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import by.htp.epam.bonjo.dao.DAOFactory;
 import by.htp.epam.bonjo.domain.Ad;
 import by.htp.epam.bonjo.domain.Category;
+import by.htp.epam.bonjo.service.AdService;
+import by.htp.epam.bonjo.service.CategoryService;
+import by.htp.epam.bonjo.service.impl.AdServiceImpl;
+import by.htp.epam.bonjo.service.impl.CategoryServiceImpl;
 import by.htp.epam.bonjo.web.command.Command;
 import by.htp.epam.bonjo.web.command.CommandName;
 
 public class MainPageCommand extends Command {
 
+	private AdService adService = new AdServiceImpl();
+	private CategoryService categoryService = new CategoryServiceImpl();
+	
 	@Override
 	public CommandName execute(HttpServletRequest request) {
-		List<Ad> ads = DAOFactory.getDaoInstance().getAdDao().readAll();
-		List<Category> categories = DAOFactory.getDaoInstance().getCategoryDao().readAll();
+		List<Ad> ads = adService.getAllAds();
+		List<Category> categories = categoryService.getAllCategories();
 		request.setAttribute("ads", ads);
 		request.setAttribute("categories", categories);
 		return CommandName.HOME;

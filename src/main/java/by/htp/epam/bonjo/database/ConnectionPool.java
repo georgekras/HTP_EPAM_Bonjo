9db56e393f6650e01520d712a69ab876a18f9cc9
George <georgekrasutski@gmail.com>
2018-08-22 14:13:17 +0300

    removed config file for connection pool

--- src/main/java/by/htp/epam/bonjo/database/ConnectionPool.java
+++ src/main/java/by/htp/epam/bonjo/database/ConnectionPool.java
@@ -3,6 +3,7 @@ package by.htp.epam.bonjo.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -12,8 +13,16 @@ import org.slf4j.LoggerFactory;
 public class ConnectionPool {
 
 	private static Logger logger = LoggerFactory.getLogger(ConnectionPool.class);
-	private static BlockingQueue<Connection> connectionQueue = new ArrayBlockingQueue<>(
-			ConnectionPoolConfiguration.POOL_SIZE, true);
+
+	private final static ResourceBundle rb = ResourceBundle.getBundle("db_config");
+
+	private final static int POOL_SIZE = 40;
+	private final static String DRIVER = rb.getString("db.driver");
+	private final static String URL = rb.getString("db.url");
+	private final static String LOGIN = rb.getString("db.login");
+	private final static String PASSWORD = rb.getString("db.pass");
+	
+	private static BlockingQueue<Connection> connectionQueue = new ArrayBlockingQueue<>(POOL_SIZE, true);
 
 	private ConnectionPool() {
 
@@ -21,17 +30,16 @@ public class ConnectionPool {
 
 	public static void connectionPoolInitialization() {
 		try {
-			Class.forName(ConnectionPoolConfiguration.DRIVER);
-			for (int i = 0; i < ConnectionPoolConfiguration.POOL_SIZE; i++) {
-				connectionQueue.add(DriverManager.getConnection(ConnectionPoolConfiguration.URL,
-						ConnectionPoolConfiguration.LOGIN, ConnectionPoolConfiguration.PASSWORD));
+			Class.forName(DRIVER);
+			for (int i = 0; i < POOL_SIZE; i++) {
+				connectionQueue.add(DriverManager.getConnection(URL, LOGIN, PASSWORD));
 			}
 		} catch (ClassNotFoundException | SQLException e) {
 			logger.error(e.getMessage() + " in initialization method of ConnectionPool class.", e);
 			System.exit(0);
 		}
 	}
-	
+
 	public static void connectionPoolDestroy() {
 		connectionQueue.removeAll(connectionQueue);
 	}

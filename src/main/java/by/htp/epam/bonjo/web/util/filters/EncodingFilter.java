64273142436f138bc43e8974b1a132d0c9e0217c
George <georgekrasutski@gmail.com>
2018-09-11 14:41:08 +0300

    encoding filter added

---
+++ src/main/java/by/htp/epam/bonjo/web/util/filters/EncodingFilter.java
@@ -0,0 +1,54 @@
+package by.htp.epam.bonjo.web.util.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * filter class for state request and response encoding
+ */
+public class EncodingFilter implements Filter {
+	/**
+	 * encoding field
+	 */
+	private String encoding;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		encoding = filterConfig.getInitParameter("encoding");
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String requestEncoding = request.getCharacterEncoding();
+		if (encoding != null && !encoding.equalsIgnoreCase(requestEncoding)) {
+			request.setCharacterEncoding(encoding);
+		}
+		String responseEncoding = response.getCharacterEncoding();
+		if (encoding != null && !encoding.equalsIgnoreCase(responseEncoding)) {
+			response.setCharacterEncoding(encoding);
+		}
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void destroy() {
+		encoding = null;
+	}
+
+}

5b38a70928bf051599919b78b085dbd8e7800bb2
George <georgekrasutski@gmail.com>
2018-09-19 23:22:26 +0300

    added new constant

--- src/main/java/by/htp/epam/bonjo/web/constants/CommandNameConstantDeclaration.java
+++ src/main/java/by/htp/epam/bonjo/web/constants/CommandNameConstantDeclaration.java
@@ -13,11 +13,12 @@ public class CommandNameConstantDeclaration {
 	public static final String COMMAND_NAME_VIEW_LOGOUT_PAGE = "logout";
 	public static final String COMMAND_NAME_VIEW_SIGNUP_PAGE = "signup";
 	public static final String COMMAND_NAME_VIEW_PROFILE_PAGE = "profile";
+	public static final String COMMAND_NAME_VIEW_ERROR_PAGE = "error";
 	public static final String COMMAND_NAME_CHANGE_LOCALE = "changelocale";
 
 	public static final String COMMAND_NAME_VIEW_CREATE_AD_PAGE = "createad";
 	public static final String COMMAND_NAME_VIEW_USER_ADS_PAGE = "userads";
-	public static final String COMMAND_NAME_VIEW_AD_PAGE = "ad";
+	public static final String COMMAND_NAME_VIEW_AD_PAGE = "viewad";
 	public static final String COMMAND_NAME_VIEW_EDIT_AD_PAGE = "editad";
 
 	public static final String COMMAND_NAME_VIEW_CREATE_CATEGORY_PAGE = "createcategory";
